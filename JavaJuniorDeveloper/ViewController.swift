716910b9b7b2da460a06dc37c071ec1bb1ecb736
iOS <ioswarrior@gmail.com>
2022-05-23 21:44:04 +0600

    Initial Commit

---
+++ JavaJuniorDeveloper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JavaJuniorDeveloper
+//
+//  Created by Muller Alexander on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
